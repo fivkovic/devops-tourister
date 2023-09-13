a7339d61bcf215b0538c650735626313af022274
Filip Ivković <f.ivkovic16@gmail.com>
2023-09-13 19:02:47 +0200

    fix(ci): correct variable specification

--- scripts/act/run-act.sh
+++ scripts/act/run-act.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # NOTE: Run this in bash shell from the root repository directory with the command: ./scripts/act/run-act.sh
 #       Don't forget to add .secrets and .env files on the paths below.
 #       For more info on how to set up act, see https://github.com/nektos/act
@@ -13,6 +15,33 @@
 #       This is because (for some reason), the caching action doesn't work properly together with act.
 #       See https://github.com/nektos/act/issues/285
 
-echo "Running act in directory:"
-pwd
-act workflow_dispatch -r --secret-file ./scripts/act/.secrets --env-file ./scripts/act/.env
+run_act_workflow() {
+    local workflow_name="$1"
+    echo "Executing $workflow_name workflow"
+    act workflow_dispatch -r --secret-file ./scripts/act/.secrets --env-file ./scripts/act/.env --var-file ./scripts/act/.vars -W ./.github/workflows/$workflow_name.yml
+}
+
+echo -n "Running act in directory: " && pwd
+
+echo "Select a workflow to execute:"
+echo "1. ci-identity-service"
+echo "2. ci-property-service"
+echo "3. ci-reservation-service"
+echo "4. ci-user-service"
+echo "5. Execute all workflows"
+
+read -p "Enter the number of the workflow you want to execute: " choice
+
+case $choice in
+    1) run_act_workflow "ci-identity-service" ;;
+    2) run_act_workflow "ci-property-service" ;;
+    3) run_act_workflow "ci-reservation-service" ;;
+    4) run_act_workflow "ci-user-service" ;;
+    5)
+        echo "Executing all workflows"
+        act workflow_dispatch -r --secret-file ./scripts/act/.secrets --env-file ./scripts/act/.env --var-file ./scripts/act/.vars
+        ;;
+    *)
+        echo "Invalid choice. Please enter a valid number (1-5)."
+        ;;
+esac
