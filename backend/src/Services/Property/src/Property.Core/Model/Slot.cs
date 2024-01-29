967582a0d6c45ac159039903acf1d56a8d7ba5b4
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-29 22:49:20 +0100

    add database model

---
+++ backend/src/Services/Property/src/Property.Core/Model/Slot.cs
@@ -0,0 +1,22 @@
+﻿namespace Property.Core.Model;
+
+public sealed class Slot
+{
+    public Guid Id { get; set; }
+    public Property Property { get; set; } = null!;
+    public bool IsAvailable { get; set; }
+    public DateOnly Start { get; set; }
+    public DateOnly End { get; set; }
+    public decimal? CustomPrice { get; set; }
+
+    public Slot() { }
+
+    public Slot(Property property, DateOnly start, DateOnly end, bool isAvailable, decimal? customPrice = null)
+    {
+        Property = property;
+        Start = start;
+        End = end;
+        IsAvailable = isAvailable;
+        CustomPrice = customPrice;
+    }
+}
