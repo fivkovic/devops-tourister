755c83584ad8e151fd04dcb363cecebc191c561e
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-29 03:43:28 +0100

    configure authentication filters

---
+++ backend/src/Shared/Security/AuthFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Shared.Security;
+
+public class AuthFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var authAttributes = context
+            .ApiDescription
+            .ActionDescriptor
+            .EndpointMetadata
+            .OfType<AuthorizeAttribute>()
+            .Distinct();
+
+        if (!authAttributes.Any()) return;
+
+        operation.Responses.TryAdd("401", new OpenApiResponse { Description = "Unauthorized" });
+        operation.Responses.TryAdd("403", new OpenApiResponse { Description = "Forbidden" });
+
+        OpenApiSecurityScheme jwtBearerScheme = new()
+        {
+            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearer" }
+        };
+
+        operation.Security = new List<OpenApiSecurityRequirement>
+        {
+            new()
+            {
+                [ jwtBearerScheme ] = Array.Empty<string>()
+            }
+        };
+    }
+}
