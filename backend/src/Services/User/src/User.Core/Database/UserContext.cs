d6b8580ef0ffa00fd27c18f9cd9062e9c90b481e
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-29 21:48:28 +0100

    configure database

---
+++ backend/src/Services/User/src/User.Core/Database/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using User.Core.Model;
+
+namespace User.Core.Database;
+
+public class UserContext
+{
+    public IMongoClient Client { get; }
+    public IMongoDatabase Database { get; }
+    public IMongoCollection<UserProfile> UserProfiles { get; }
+
+    public UserContext(IMongoClient client, string database)
+    {
+        Client = client;
+        Database = client.GetDatabase(database);
+        UserProfiles = Database.GetCollection<UserProfile>(nameof(UserProfiles));
+    }
+}
