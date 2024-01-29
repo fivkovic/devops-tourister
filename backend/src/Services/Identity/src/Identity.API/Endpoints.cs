2bab2cf988473e29f8e1a36bba6c35686ce10747
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-29 03:49:12 +0100

    feat: user authentication
    * update dependencies
    
    * implement JWT creation
    
    * implement SignIn command handler
    
    * configure authentication filters
    
    * add authentication to OpenAPI
    
    * add support for JWT and CORS handling
    
    * implement '/authentication/signin' API endpoint

--- backend/src/Services/Identity/src/Identity.API/Endpoints.cs
+++ backend/src/Services/Identity/src/Identity.API/Endpoints.cs
@@ -11,9 +11,14 @@ public sealed class Endpoints
     public static void Map(IEndpointRouteBuilder routes)
     {
         routes.MapPost(_prefix + "/signup", SignUp)
-              .Produces(StatusCodes.Status200OK)
-              .ProducesValidationProblem()
-              .ProducesProblem(StatusCodes.Status400BadRequest);
+            .Produces(StatusCodes.Status200OK)
+            .ProducesValidationProblem()
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+
+        routes.MapPost(_prefix + "/signin", SignIn)
+            .Produces<SignInResponse>()
+            .ProducesValidationProblem()
+            .ProducesProblem(StatusCodes.Status400BadRequest);
     }
 
     /// <summary>
@@ -48,4 +53,46 @@ public sealed class Endpoints
 
         return Results.Problem(error, "sign-up-failed", StatusCodes.Status400BadRequest);
     }
+
+    /// <summary>
+    /// Signs in a user using email and password and returns a JWT token.
+    /// </summary>
+    /// <response code="200">Returns a <see cref="SignInResponse"/> instance when the sign in is successful</response>
+    /// <response code="400">
+    /// Returns a <see cref="ValidationProblemDetails"/> instance when the request fields are invalid <br></br>
+    /// Returns a <see cref="ProblemDetails"/> instance when a user with the provided email does not exist <br></br>
+    /// Returns a <see cref="ProblemDetails"/> instance when the provided password is incorrect <br></br>
+    /// [ProblemDetails.Instance = "invalid-credentials"]
+    /// </response>
+    private static async Task<IResult> SignIn(
+        SignInRequest request,
+        IMediator mediator,
+        ILogger<Endpoints> logger,
+        CancellationToken cancellationToken
+    )
+    {
+        var validation = request.Validate();
+        if (!validation.IsValid) return Results.ValidationProblem(validation.ToDictionary());
+
+        logger.LogInformation("Handling sign in request for email: {Email}", request.Email);
+
+        var response = await mediator.Send(request, cancellationToken);
+        if (response.IsSuccess)
+        {
+            logger.LogInformation("Successfully signed in user with email: {Email}", request.Email);
+            return Results.Ok(response.Value);
+        }
+
+        var error = response.Errors.First().Message;
+        var message = error switch
+        {
+            SignInError.EmailNotFound => "The email you entered doesn't belong to an account. Please check your email and try again.",
+            SignInError.IncorrectPassword or _ => "Sorry, your password was incorrect. Please double-check your password.",
+        };
+
+        logger.LogWarning("Sign in request failed for email: {Email}. Error={Error}.", request.Email, error);
+
+        return Results.Problem(message, "invalid-credentials", StatusCodes.Status400BadRequest);
+    }
+
 }
---
+++ backend/src/Shared/Security/KeyProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Shared.Security;
+
+public static class KeyProvider
+{
+    private const string _developmentJwtSecret = "/wMLPRs1zvFxIsIztaakWJq7TWF+Lg==";
+
+    public const string Issuer = "https://api.tourister.com";
+    public const string Audience = "https://tourister.com";
+
+    public static readonly SigningCredentials SigningCredentials;
+
+    static KeyProvider()
+    {
+        var secret = Environment.GetEnvironmentVariable("JWT_SECRET") ?? _developmentJwtSecret;
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+        SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+    }
+}
