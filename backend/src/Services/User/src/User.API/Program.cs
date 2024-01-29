4f889b356e1d8bbb2e2b1306b375c55f5d0ced09
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-29 22:00:16 +0100

    implement consumer for UserRegistered event

--- backend/src/Services/User/src/User.API/Program.cs
+++ backend/src/Services/User/src/User.API/Program.cs
@@ -1,4 +1,40 @@
+using MassTransit;
+using Mediator;
+using MongoDB.Driver;
+using User.Core.Consumers;
+using User.Core.Database;
+
+[assembly: MediatorOptions(Namespace = "User.API", ServiceLifetime = ServiceLifetime.Scoped)]
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMediator();
+
+builder.Services.AddSingleton(o =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION") ??
+                           builder.Configuration.GetConnectionString("MongoDB");
+
+    var client = new MongoClient(connectionString);
+
+    return new UserContext(client, database: "user");
+});
+
+builder.Services.AddMassTransit(config =>
+{
+    config.SetEndpointNameFormatter(new DefaultEndpointNameFormatter(includeNamespace: true));
+
+    config.AddConsumer<CreateUser>();
+
+    config.UsingRabbitMq((context, cfg) =>
+    {
+        var connectionString = Environment.GetEnvironmentVariable("RABBITMQ_CONNECTION") ??
+                               builder.Configuration.GetConnectionString("RabbitMQ");
+
+        cfg.Host(new Uri(connectionString!));
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ backend/src/Services/User/src/User.Core/Events/IntegrationEvents.cs
@@ -0,0 +1,10 @@
+﻿namespace User.Core.Events;
+
+public sealed record UserRegistered(
+    Guid UserId,
+    string Email,
+    string FirstName,
+    string LastName,
+    string Role,
+    string Residence
+);
