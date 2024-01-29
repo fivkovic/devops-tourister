878a992634eb26e62b389f67bb38d1833204861d
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-29 03:10:11 +0100

    feat: account registration
    * update dependencies and add User model
    
    * remove empty directories
    
    * configure DbContext and add migrations
    
    * implement SignUp command handler
    
    * add shared OpenAPI configuration
    
    * implement '/authentication/signup' API endpoint

--- backend/src/Services/Identity/src/Identity.API/Program.cs
+++ backend/src/Services/Identity/src/Identity.API/Program.cs
@@ -1,6 +1,64 @@
+using FluentValidation;
+using Identity.API;
+using Identity.Core.Database;
+using Identity.Core.Model;
+using Mediator;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Shared.Swagger;
+
+[assembly: MediatorOptions(Namespace = "Identity.API", ServiceLifetime = ServiceLifetime.Scoped)]
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddMediator();
+
+builder.Services.AddDbContextPool<IdentityContext>(options =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("PG_CONNECTION") ??
+        builder.Configuration.GetConnectionString("Postgres");
+
+    options.UseNpgsql(connectionString);
+});
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequiredLength = 6;
+    options.User.RequireUniqueEmail = true;
+})
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddDefaultTokenProviders();
+
+// Add Authorization
+builder.Services.AddAuthorization();
+
+// Add Swagger
+builder.Services.AddSwagger("Identity.API");
+
+// Add FluentValidation to Swagger
+builder.Services.AddValidatorsFromAssemblyContaining<IdentityContext>();
+builder.Services.AddFluentValidationRulesToSwagger();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<IdentityContext>();
+    var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+    if (pendingMigrations.Any())
+        await context.Database.MigrateAsync();
+}
+
+Endpoints.Map(app);
 
 app.Run();
---
+++ backend/src/Shared/Swagger/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Shared.Swagger;
+
+public static class Extensions
+{
+    public static void AddSwagger(this IServiceCollection services, string serviceName)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.CustomSchemaIds(type => type.ToString().Replace("+", "."));
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = serviceName, Version = "v1" });
+        });
+    }
+}
