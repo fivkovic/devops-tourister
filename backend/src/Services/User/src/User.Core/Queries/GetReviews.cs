cd34ca1d1680e5a51f70f0eb1cce8651c7b352b4
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-30 02:40:54 +0100

    implement GetReviews query handler

---
+++ backend/src/Services/User/src/User.Core/Queries/GetReviews.cs
@@ -0,0 +1,36 @@
+﻿using FluentResults;
+using Mediator;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using User.Core.Database;
+
+namespace User.Core.Queries;
+
+using User.Core.Model;
+
+public class GetReviews
+{
+    public record Query(Guid UserId) : IQuery<Result<HostReviews>>;
+
+    public record HostReviews(UserProfile Host, IQueryable<Review> Reviews);
+
+    public class HostNotFound() : Error("Host not found");
+
+    public class Handler(UserContext context) : IQueryHandler<Query, Result<HostReviews>>
+    {
+        public async ValueTask<Result<HostReviews>> Handle(Query query, CancellationToken cancellationToken)
+        {
+            var host = await context.UserProfiles
+                .Find(u => u.Id == query.UserId)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (host is null) return new HostNotFound();
+
+            var reviews = context.Reviews
+                .AsQueryable()
+                .Where(r => r.OwnerId == query.UserId);
+
+            return new HostReviews(host, reviews);
+        }
+    }
+}
