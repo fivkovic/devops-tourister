c220beee78636846321f593375ae81246eca8766
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-30 05:51:40 +0100

    fix: add missing subscription settings endpoint
    * implement GetSubscriptionSettings query handler
    
    * implement GET on '/reservations/notifications/settings' API endpoint

---
+++ backend/src/Services/Reservation/src/Reservation.Core/Queries/GetSubscriptionSettings.cs
@@ -0,0 +1,28 @@
+﻿using FluentResults;
+using Mediator;
+using MongoDB.Driver;
+using Reservation.Core.Database;
+using Reservation.Core.Model;
+
+namespace Reservation.Core.Queries;
+
+public static class GetSubscriptionSettings
+{
+    public record Query(Guid UserId) : IQuery<Result<Subscription>>;
+
+    public class UserNotFound() : Error("User not found");
+
+    public class Handler(ReservationContext context) : IQueryHandler<Query, Result<Subscription>>
+    {
+        public async ValueTask<Result<Subscription>> Handle(Query query, CancellationToken cancellationToken)
+        {
+            var user = await context.Users
+                .Find(u => u.Id == query.UserId)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (user is null) return new UserNotFound();
+
+            return user.Subscription;
+        }
+    }
+}
