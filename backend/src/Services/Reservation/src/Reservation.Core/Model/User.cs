7ae10006d290279ccd326ad4c0daa6abd7a8d004
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-30 01:17:03 +0100

    add User model

---
+++ backend/src/Services/Reservation/src/Reservation.Core/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Reservation.Core.Model;
+
+public sealed class User
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
+    public string Role { get; set; }
+    public int ReservationsCancelled { get; set; }
+}
