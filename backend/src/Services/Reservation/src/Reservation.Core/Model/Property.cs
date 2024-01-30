96391b7dd1ef3d8cd275859a4cead083fcc5cb37
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-30 01:17:14 +0100

    add Property model

---
+++ backend/src/Services/Reservation/src/Reservation.Core/Model/Property.cs
@@ -0,0 +1,11 @@
+﻿namespace Reservation.Core.Model;
+
+public sealed class Property
+{
+    public Guid Id { get; set; }
+    public Guid OwnerId { get; set; }
+    public User Owner { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string[] Images { get; set; } = [];
+}
