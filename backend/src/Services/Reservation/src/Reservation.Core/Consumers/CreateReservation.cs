9e20c080b4adceed38999d16ff98134eee73ca4e
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-30 01:19:14 +0100

    implement consumer for ReservationRequested event

---
+++ backend/src/Services/Reservation/src/Reservation.Core/Consumers/CreateReservation.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Reservation.Core.Database;
+using Reservation.Core.Events;
+
+namespace Reservation.Core.Consumers;
+
+public sealed class CreateReservation(
+    ReservationContext context,
+    ILogger<CreateReservation> logger) : IConsumer<ReservationRequested>
+{
+    public async Task Consume(ConsumeContext<ReservationRequested> consumer)
+    {
+        var reservation = consumer.Message.Reservation;
+        await context.Reservations.InsertOneAsync(reservation);
+
+        logger.LogInformation("Reservation {ReservationId} created by {UserId}",
+                               reservation.Id, reservation.Customer.Id);
+
+        if (reservation.IsAccepted)
+        {
+            var @event = new ReservationAccepted(reservation.Id);
+            await consumer.Publish(@event);
+            logger.LogInformation("Reservation {ReservationId} will be automatically accepted for customer {CustomerId}",
+                                   reservation.Id, reservation.Customer.Id);
+        }
+    }
+}
