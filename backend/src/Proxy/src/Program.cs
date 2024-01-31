36b82ad4273276c204e4b57c0e4fc43c78044c41
Filip Ivković <f.ivkovic16@gmail.com>
2024-01-31 02:33:19 +0100

    configure OpenTelemetry metrics and Prometheus exporter

--- backend/src/Proxy/src/Program.cs
+++ backend/src/Proxy/src/Program.cs
@@ -1,3 +1,4 @@
+using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
@@ -9,7 +10,7 @@ builder.Services
 
 builder.Services
     .AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService("Gateway"))
+    .ConfigureResource(resource => resource.AddService("Proxy"))
     .WithTracing(config =>
     {
         // Listen to the YARP tracing activities
@@ -23,9 +24,29 @@ builder.Services
 
                 c.Endpoint = new Uri(endpoint!);
             });
+    })
+    .WithMetrics(config =>
+    {
+        config.AddPrometheusExporter()
+              .AddMeter("Microsoft.AspNetCore.Hosting")
+              .AddMeter("Microsoft.AspNetCore.Routing")
+              .AddMeter("Microsoft.AspNetCore.Diagnostics")
+              .AddMeter("Microsoft.AspNetCore.RateLimiting")
+              .AddMeter("Microsoft.AspNetCore.HeaderParsing")
+              .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+              .AddMeter("Microsoft.AspNetCore.Http.Connections")
+              .AddView(
+                    instrumentName: "http.server.request.duration",
+                    metricStreamConfiguration: new ExplicitBucketHistogramConfiguration
+                    {
+                        Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
+                    });
     });
 
 var app = builder.Build();
 
+app.MapPrometheusScrapingEndpoint();
+
 app.MapReverseProxy();
+
 app.Run();
